f34b3ac0967f1dbdc5600a0d67de377f0fafd428
Johannes Huning <mail@johanneshuning.com>
2015-10-08 11:43:26 +0200

    remove fun-indirection

--- src/nested.erl
+++ src/nested.erl
@@ -2,12 +2,12 @@
 
 -export([
     is_key/2,
-    put/3, putf/1,
-    get/2, getf/1,
-    get/3, getf/2,
-    update/3, updatef/1,
-    remove/2, removef/1,
-    keys/2, keysf/1,
+    put/3,
+    get/2,
+    get/3,
+    update/3,
+    remove/2,
+    keys/2,
     append/3
 ]).
 
@@ -21,43 +21,25 @@ is_key([Key|PathRest], Map) ->
     end.
 
 
-get(Path, Map) ->
-    GetFun = getf(Path),
-    GetFun(Map).
+get([Key|PathRest], Map) ->
+    get(PathRest, maps:get(Key, Map));
 
-getf(Path) ->
-    fun(Map) -> getf_internal(Path, Map) end.
-
-getf_internal([Key|PathRest], Map) ->
-    getf_internal(PathRest, maps:get(Key, Map));
-getf_internal([], Value) ->
+get([], Value) ->
     Value.
 
-get(Path, Map, Default) ->
-    GetFun = getf(Path, Default),
-    GetFun(Map).
+get([Key|PathRest], Map, Default) ->
+    get(PathRest, maps:get(Key, Map, Default), Default);
 
-getf(Path, Default) ->
-    fun(Map) -> getf_internal(Path, Map, Default) end.
-
-getf_internal([Key|PathRest], Map, Default) ->
-    getf_internal(PathRest, maps:get(Key, Map, Default), Default);
-getf_internal([], Value, _) ->
+get([], Value, _) ->
     Value.
 
 update(Path, ValueOrFun, Map) ->
-    SetFun = updatef(Path),
-    SetFun(ValueOrFun, Map).
-
-updatef(Path) ->
-    fun(ValueOrFun, Map) ->
-        try updatef_internal(Path, ValueOrFun, Map)
-        catch
-            error:{error, {no_map, PathRest, Element}} ->
-                PathLength  = length(Path) - length(PathRest),
-                PathToThrow = lists:sublist(Path, PathLength),
-                erlang:error({no_map, PathToThrow, Element})
-        end
+    try updatef_internal(Path, ValueOrFun, Map)
+    catch
+        error:{error, {no_map, PathRest, Element}} ->
+            PathLength  = length(Path) - length(PathRest),
+            PathToThrow = lists:sublist(Path, PathLength),
+            erlang:error({no_map, PathToThrow, Element})
     end.
 
 updatef_internal([Key|PathRest], ValueOrFun, Map) when is_map(Map) ->
@@ -70,52 +52,31 @@ updatef_internal(Path, _, Element) ->
     erlang:error({error, {no_map, Path, Element}}).
 
 
-put(Path, Value, Map) ->
-    PutFun = putf(Path),
-    PutFun(Value, Map).
-
-putf(Path) ->
-    fun(Value, Map) -> putf_internal(Path, Value, Map) end.
-
-putf_internal([Key|PathRest], Value, Map) ->
+put([Key|PathRest], Value, Map) ->
     SubMap =
     case maps:is_key(Key, Map) andalso is_map(maps:get(Key, Map)) of
        true ->  maps:get(Key, Map);
        false -> #{}
     end,
-    maps:put(Key, putf_internal(PathRest, Value, SubMap), Map);
-putf_internal([], Value, _) ->
+    maps:put(Key, put(PathRest, Value, SubMap), Map);
+put([], Value, _) ->
     Value.
 
-remove(Path, Map) ->
-    RemoveFun = removef(Path),
-    RemoveFun(Map).
-
-removef(Path) ->
-    fun(Map) -> removef_internal(Path, Map) end.
-
-removef_internal([], _) ->
+remove([], _) ->
     throw({bad_path, []});
-removef_internal([LastKey], Map) ->
+remove([LastKey], Map) ->
     maps:remove(LastKey, Map);
-removef_internal([Key|PathRest], Map) ->
+remove([Key|PathRest], Map) ->
     case maps:is_key(Key, Map) of
         true ->
-            maps:put(Key, removef_internal(PathRest, maps:get(Key, Map)), Map);
+            maps:put(Key, remove(PathRest, maps:get(Key, Map)), Map);
         false ->
             Map
     end.
 
-keys(Path, Map) ->
-    KeysFun = keysf(Path),
-    KeysFun(Map).
-
-keysf(Path) ->
-    fun(Map) -> keysf_internal(Path, Map) end.
-
-keysf_internal([Key|PathRest], Map) ->
-    keysf_internal(PathRest, maps:get(Key, Map));
-keysf_internal([], Map) ->
+keys([Key|PathRest], Map) ->
+    keys(PathRest, maps:get(Key, Map));
+keys([], Map) ->
     maps:keys(Map).
 
 append(Path, Value, Map) ->
